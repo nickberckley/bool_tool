f4c2f08c335f2eacc0319289807d15c9e148864c
Nika Kutsniashvili <nickberckley@gmail.com>
2026-07-14 16:36:37 +0400

    Update blender_manifest

--- source/manual.py
+++ source/manual.py
@@ -4,31 +4,34 @@ import bpy
 #### ------------------------------ FUNCTIONS ------------------------------ ####
 
 def bool_tool_manual_map():
-    url_manual_prefix = "https://github.com/nickberckley/bool_tool/wiki/"
+    url_manual_prefix = "https://nickberckley.github.io/bool_tool/"
 
     # Carver
-    url_manual_mapping = (("bpy.ops.object.carve", "Carver"),
-                          # Brush Boolean
-                          ("bpy.ops.object.boolean_brush_union", "Boolean-Operators"),
-                          ("bpy.ops.object.boolean_brush_intersect", "Boolean-Operators"),
-                          ("bpy.ops.object.boolean_brush_difference", "Boolean-Operators"),
-                          ("bpy.ops.object.boolean_brush_slice", "Boolean-Operators"),
+    url_manual_mapping = (# Brush Boolean
+                          ("bpy.ops.object.boolean_brush_union", "booleans/brush_booleans.html"),
+                          ("bpy.ops.object.boolean_brush_intersect", "booleans/brush_booleans.html"),
+                          ("bpy.ops.object.boolean_brush_difference", "booleans/brush_booleans.html"),
+                          ("bpy.ops.object.boolean_brush_slice", "booleans/brush_booleans.html"),
                           # Auto Boolean
-                          ("bpy.ops.object.boolean_auto_union", "Boolean-Operators#auto-boolean-operators"),
-                          ("bpy.ops.object.boolean_auto_intersect", "Boolean-Operators#auto-boolean-operators"),
-                          ("bpy.ops.object.boolean_auto_difference", "Boolean-Operators#auto-boolean-operators"),
-                          ("bpy.ops.object.boolean_auto_slice", "Boolean-Operators#auto-boolean-operators"),
+                          ("bpy.ops.object.boolean_auto_union", "booleans/auto_booleans.html"),
+                          ("bpy.ops.object.boolean_auto_intersect", "booleans/auto_booleans.html"),
+                          ("bpy.ops.object.boolean_auto_difference", "booleans/auto_booleans.html"),
+                          ("bpy.ops.object.boolean_auto_slice", "booleans/auto_booleans.html"),
+                          # Carver
+                          ("bpy.ops.object.carve_box", "carver/index.html"),
+                          ("bpy.ops.object.carve_circle", "carver/index.html"),
+                          ("bpy.ops.object.carve_polyline", "carver/index.html"),
                           # Cutter Utilities
-                          ("bpy.ops.object.boolean_toggle_cutter", "Utility-Operators#toggle-cutter"),
-                          ("bpy.ops.object.boolean_remove_cutter", "Utility-Operators#remove-cutter"),
-                          ("bpy.ops.object.boolean_apply_cutter", "Utility-Operators#apply-cutter"),
+                          ("bpy.ops.object.boolean_toggle_cutter", "utilities/toggle.html"),
+                          ("bpy.ops.object.boolean_remove_cutter", "utilities/remove.html"),
+                          ("bpy.ops.object.boolean_apply_cutter", "utilities/apply.html"),
                           # Canvas Utilities
-                          ("bpy.ops.object.boolean_toggle_all", "Utility-Operators#toggle-all-cutters"),
-                          ("bpy.ops.object.boolean_remove_all", "Utility-Operators#remove-all-cutters"),
-                          ("bpy.ops.object.boolean_apply_all", "Utility-Operators#apply-all-cutters"),
+                          ("bpy.ops.object.boolean_toggle_all", "utilities/toggle.html"),
+                          ("bpy.ops.object.boolean_remove_all", "utilities/remove.html"),
+                          ("bpy.ops.object.boolean_apply_all", "utilities/apply.html"),
                           # Select
-                          ("bpy.ops.object.select_cutter_canvas", "Utility-Operators#select-operators"),
-                          ("bpy.ops.object.boolean_select_all", "Utility-Operators#select-operators"),
+                          ("bpy.ops.object.select_cutter_canvas", "utilities/select.html"),
+                          ("bpy.ops.object.boolean_select_all", "utilities/select.html"),
     )
 
     return url_manual_prefix, url_manual_mapping
